9a524e016ab9629ead0b6df0627ab111580ae988
7563Yui7564 <takashi.t1001@gmail.com>
2021-08-08 22:34:40 +0900

    バリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          validates :nickname, presence: true
+         # パスワードは、半角英数字混合での入力が必須であること
+         validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i,
+           message: "is invalid. Input half-width characters." }
 end
