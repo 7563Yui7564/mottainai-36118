bf91a92c94e840d02ae58e80d9de89ba10abbc97
7563Yui7564 <takashi.t1001@gmail.com>
2021-08-11 16:40:42 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: "creations#index"
   devise_for :users
-  resources :creations, only: [:index, :new, :create]
+  resources :creations, only: [:index, :new, :create, :show]
 end
